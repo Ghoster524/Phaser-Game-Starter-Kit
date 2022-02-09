563679703347b4fd816e99e1ac67b2c129283b53
martijnzentjens <martijnzentjens@livewallgroup.nl>
2022-02-09 10:14:13 +0100

    feat(section2): Display shape, load google font & use group

---
+++ public/assets/scenes/LevelX_template.js
@@ -0,0 +1,18 @@
+export default class Level1 extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'LevelX_template',
+        });
+    }
+
+    preload() {
+        //
+    }
+
+    create() {
+        //
+    }
+
+    update(time, delta) {
+    }
+}
