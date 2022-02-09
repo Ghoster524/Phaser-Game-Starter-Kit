ef8d74be5f1e51c7ecaeebaa21a5a4592c2e08d1
martijnzentjens <martijnzentjens@livewallgroup.nl>
2022-02-09 11:51:09 +0100

    feat(section3): Created my first tilemap

--- public/assets/scenes/section3/Level3.js
+++ public/assets/scenes/section3/Level3.js
@@ -6,12 +6,22 @@ export default class Level3 extends Phaser.Scene {
     }
 
     preload() {
-        //
+        this.load.image('platform_tiles', '/assets/tiles/arcade_platformerV2-transparent.png');
+        this.load.tilemapTiledJSON('map', '/assets/tiles/2dgame/2dgame.json');
     }
 
     create() {
-        console.log(1);
-        //
+        const map = this.make.tilemap({
+            key: 'map',
+            tileWidth: 16,
+            tileHeight: 16,
+        });
+        const tileset = map.addTilesetImage('2dgame_tileset', 'platform_tiles');
+
+        const backgroundLayer = map.createStaticLayer('background', tileset, 0, 0);
+        const treeLayer = map.createStaticLayer('trees', tileset, 0, 0);
+        const grassLayer = map.createStaticLayer('grass', tileset, 0, 0);
+        const terrainLayer = map.createStaticLayer('terrain', tileset, 0, 0);
     }
 
     update(time, delta) {
