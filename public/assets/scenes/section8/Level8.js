cb106772003820da9af463a3dc62f60ce2cf6f24
martijnzentjens <martijnzentjens@livewallgroup.nl>
2022-02-10 11:06:51 +0100

    feat(audio): Imported audio music

--- public/assets/scenes/section8/Level8.js
+++ public/assets/scenes/section8/Level8.js
@@ -1,3 +1,5 @@
+const musicKey = 'bgmusic';
+
 export default class Level1 extends Phaser.Scene {
     constructor() {
         super({
@@ -6,12 +8,24 @@ export default class Level1 extends Phaser.Scene {
     }
 
     preload() {
-        //
+        this.load.audio(musicKey, ['/assets/audio/first-audio.ogg']);
     }
 
     create() {
         console.log(1);
-        //
+        this.bgmusic = this.sound.add(musicKey, {
+            mute: false,
+            volume: 0.5,
+        });
+        this.bgmusic.play();
+
+        // basic methodes
+        this.bgmusic.setVolume(0.25);
+        // this.bgmusic.pause();
+        // this.bgmusic.resume();
+        // this.bgmusic.setMute(true or false);
+        // this.bgmusic.setDetune(-500);
+        // this.bgmusic.setLoop(true or false);
     }
 
     update(time, delta) {
