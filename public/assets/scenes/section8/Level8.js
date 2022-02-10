30133005081c659fa9ba228c290fa8af8c749491
martijnzentjens <martijnzentjens@livewallgroup.nl>
2022-02-10 10:33:29 +0100

    refactor(section6): Preparing before starting section 8

---
+++ public/assets/scenes/section8/Level8.js
@@ -0,0 +1,19 @@
+export default class Level1 extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'Level8',
+        });
+    }
+
+    preload() {
+        //
+    }
+
+    create() {
+        console.log(1);
+        //
+    }
+
+    update(time, delta) {
+    }
+}
